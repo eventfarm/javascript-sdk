80766c2b0b8d7244c706ab6c2cd603df8840a5d0
Barry Ross <barry.ross@eventfarm.com>
2023-05-31 14:44:15 -0400

    Improves methodology of handling expired tokens

--- src/Rest/Server/ServerAccessToken.ts
+++ src/Rest/Server/ServerAccessToken.ts
@@ -24,7 +24,7 @@ export class ServerAccessToken {
   }
 
   get isExpired(): boolean {
-    let currentTime = new Date().getSeconds();
+    let currentTime = Math.floor(Date.now() / 1000)
     return currentTime > this._expiresAt;
   }
 }
